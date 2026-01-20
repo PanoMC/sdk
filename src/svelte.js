d6276a4453cfb07f9459794b3921e4f1a6a67a94
kahverengi <ahmet@duruer.dev>
2026-01-20 23:32:28 +0300

    feat: added support for error of sveltekit wrapping

--- src/svelte.js
+++ src/svelte.js
@@ -1,6 +1,6 @@
 import { getPanoContext } from './internal/index.js';
 
 const panoContext = getPanoContext();
-const { page, base, navigating, browser, goto, invalidate, invalidateAll } = panoContext.context;
+const { page, base, navigating, browser, goto, invalidate, invalidateAll, error } = panoContext.context;
 
-export { page, base, navigating, browser, goto, invalidate, invalidateAll };
+export { page, base, navigating, browser, goto, invalidate, invalidateAll, error };
